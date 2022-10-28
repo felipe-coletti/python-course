b3becbeed72a8272d7b6abe15a795c9e0f1b1b1a
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-28 17:15:44 -0300

    Update exercise-007.py

--- module-001/exercise-007.py
+++ module-001/exercise-007.py
@@ -3,4 +3,4 @@ n2 = float(input('Digite a segunda nota: '))
 
 m = (n1 + n2) / 2
 
-print('A média entre {} e {} é igual a {}.'.format(n1, n2, m))
+print('A média entre {:.2f} e {:.2f} é igual a {:.2f}.'.format(n1, n2, m))
