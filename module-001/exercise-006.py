17e9c53142a0b98f511555c768fee19ae661aa16
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-28 17:03:55 -0300

    Create exercise-006.py

---
+++ module-001/exercise-006.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+n = int(input('Digite um número: '))
+
+d = n * 2
+t = n / 2
+r = sqrt(n)
+
+print('O dobro de {} é {}.'.format(d))
+print('O triplo de {} é {}.'.format(t))
+print('A raiz quadrada de {} é {}.'.format(r))
