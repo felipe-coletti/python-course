0947f35aa587ac247f661eabfe8ce8762209c185
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-28 17:01:47 -0300

    Update exercise-005.py

--- module-001/exercise-005.py
+++ module-001/exercise-005.py
@@ -3,4 +3,5 @@ n = int(input('Digite um número: '))
 a = n - 1
 s = n + 1
 
-print('O antecessor é {} e o sucessor é {}.'.format(a, s))
+print('O antecessor de {} é {}.'.format(n, a))
+print('O sucessor de {} é {}.'.format(n, s))
