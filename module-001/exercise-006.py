8ec23928c93a6fca5f52af394040f7ab015b8746
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-28 17:10:09 -0300

    Update exercise-006.py

--- module-001/exercise-006.py
+++ module-001/exercise-006.py
@@ -4,6 +4,6 @@ d = n * 2
 t = n * 3
 r = n**(1/2)
 
-print('O dobro de {} é {}.'.format(d))
-print('O triplo de {} é {}.'.format(t))
-print('A raiz quadrada de {} é {}.'.format(r))
+print('O dobro de {} é {}.'.format(n, d))
+print('O triplo de {} é {}.'.format(n, t))
+print('A raiz quadrada de {} é {}.'.format(n, r))
