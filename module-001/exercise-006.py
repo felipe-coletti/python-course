fef6c717f3106097eca1118ba6139a0b00e22e69
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-28 17:06:49 -0300

    Update exercise-006.py

--- module-001/exercise-006.py
+++ module-001/exercise-006.py
@@ -1,10 +1,8 @@
-from math import sqrt
-
 n = int(input('Digite um número: '))
 
 d = n * 2
 t = n * 3
-r = sqrt(n)
+r = n ** (1/2)
 
 print('O dobro de {} é {}.'.format(d))
 print('O triplo de {} é {}.'.format(t))
