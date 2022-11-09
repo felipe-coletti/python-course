3cd725718a735fec8e394d447afd84dc2e209b7e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-08 22:51:46 -0300

    Create exercise-008.py

---
+++ module-001/exercise-008.py
@@ -0,0 +1,10 @@
+m = int(input('Digite um valor em metros: '))
+
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+
+print('Isso equivale a:\n{}km\n{}hm\n{}dam\n{}dm\n{}cm\n{}mm '.format(km, hm, dam, dm, cm, mm))
