b59307abf3ebd4034bdb1aea39bc6f7ef2143dc7
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-23 19:53:49 -0300

    Update exercise-011.py

--- module-001/exercise-011.py
+++ module-001/exercise-011.py
@@ -5,5 +5,5 @@ a = b * h
 
 l = a / 2
 
-print('A área de uma parede com as dimensões {}x{} é de {} m².'.format(b, h, a))
-print('Para pinta-la, será necessário {} L de tinta.'.format(l))
+print('A área de uma parede com as dimensões {:.2f}x{:.2f} é de {:.2f} m².'.format(b, h, a))
+print('Para pinta-la, será necessário {:.2f} L de tinta.'.format(l))
