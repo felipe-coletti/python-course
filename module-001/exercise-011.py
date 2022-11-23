4d952f4f53419999ec1a13f8baf0f2202b603deb
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-23 12:38:42 -0300

    Create exercise-011.py

---
+++ module-001/exercise-011.py
@@ -0,0 +1,9 @@
+b = float(input('Digite a largura da parede: '))
+h = float(input('Digite a altura da parede: '))
+
+a = b * h
+
+l = a / 2
+
+print('A área da parede é de {}m².'.format(a))
+print('Para pinta-la, será necessário {}L de tinta.'.format(l))
