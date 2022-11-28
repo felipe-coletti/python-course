c013e6d235deea32b99513d93654837c7cd466f6
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 13:14:44 -0300

    Update exercise-013.py

--- module-001/exercise-013.py
+++ module-001/exercise-013.py
@@ -1,3 +1,7 @@
+'''
+Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+'''
+
 s = float(input('Digite o salário de um funcionário: R$ '))
 
 n = s + (15 / 100 * s)
