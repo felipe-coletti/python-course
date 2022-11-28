9ebd748c2d7a11be6352cf1f771381d18ca03561
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 13:31:31 -0300

    Update exercise-014.py

--- module-001/exercise-014.py
+++ module-001/exercise-014.py
@@ -5,5 +5,6 @@ Escreva um programa que leia uma temperatura em graus Celsius e converta para gr
 c = float(input('Digite uma temperatura em ºC: '))
 
 f = (c * 9 / 5) + 32
+k = c + 273.15
 
-print('{:.2f} ºC equivale a {:.2f} ºF.'.format(c, f))
+print('{:.2f} ºC equivale a {:.2f} ºF e {:.2f} ºK.'.format(c, f, k))
