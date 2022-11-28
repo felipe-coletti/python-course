d5a18585b15bcf4151a058b1b04bbe4733493d40
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 13:35:26 -0300

    Update exercise-010.py

--- module-001/exercise-010.py
+++ module-001/exercise-010.py
@@ -1,5 +1,5 @@
 '''
-Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere USD$ 1,00 = R$ 3,27.
 '''
 
 r = float(input('Digite um valor em reais: '))
