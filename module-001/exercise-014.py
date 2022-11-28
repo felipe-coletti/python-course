de70e03f4f7109df301b00af1b931ba36144c90e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 13:38:41 -0300

    Update exercise-014.py

--- module-001/exercise-014.py
+++ module-001/exercise-014.py
@@ -2,9 +2,9 @@
 Escreva um programa que leia uma temperatura em graus Celsius e converta para graus Fahrenheit.
 '''
 
-c = float(input('Digite uma temperatura em ºC: '))
+c = float(input('Digite uma temperatura em °C: '))
 
 f = (c * 9 / 5) + 32
 k = c + 273.15
 
-print('{:.2f} ºC equivale a {:.2f} ºF e {:.2f} ºK.'.format(c, f, k))
+print('{:.2f} °C equivale a {:.2f} °F e {:.2f} °K.'.format(c, f, k))
