9d6328634916d32aa0eb1ccf6b490322b9f690cd
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 13:12:13 -0300

    Update exercise-012.py

--- module-001/exercise-012.py
+++ module-001/exercise-012.py
@@ -1,3 +1,7 @@
+'''
+Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+'''
+
 p = float(input('Digite o preço do produto: R$ '))
 
 n = p - (5 / 100 * p)
