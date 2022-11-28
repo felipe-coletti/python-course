502a907be11bd5b62a8f201206cc67f174335d15
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 01:08:39 -0300

    Update exercise-003.py

--- module-001/exercise-003.py
+++ module-001/exercise-003.py
@@ -1,6 +1,10 @@
-v1 = int(input('Digite o primeiro valor: '))
-v2 = int(input('Digite o segundo valor: '))
+'''
+Crie um programa que leia dois números e mostre a soma entre eles.
+'''
+
+v1 = float(input('Digite o primeiro valor: '))
+v2 = float(input('Digite o segundo valor: '))
 
 s = v1 + v2
 
-print('A soma entre {} e {} é igual a {}.'.format(v1, v2, s))
+print('A soma entre {:.2f} e {:.2f} é igual a {:.2f}.'.format(v1, v2, s))
