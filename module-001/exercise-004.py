76e0aeaa0656a286b1529b8cfa6b14093d4879ad
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-28 12:58:38 -0300

    Update exercise-004.py

--- module-001/exercise-004.py
+++ module-001/exercise-004.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+'''
+
 x = input('Digite algo: ')
 
 print('O tipo primitivo é: {}'.format(type(x)))
