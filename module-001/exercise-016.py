b6220c40f62b76cec91436f8d5f9a6879d48efa0
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-21 21:59:24 -0300

    Update exercise-016.py

--- module-001/exercise-016.py
+++ module-001/exercise-016.py
@@ -1,6 +1,8 @@
 '''
-Crie um programa que leia um número real qualquer e mostra na tela a sua porção inteira
+Crie um programa que leia um número real qualquer e mostra na tela a sua porção inteira.
 '''
+
+import math
 
 n = float(input('Digite um número: '))
 
