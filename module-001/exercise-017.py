f81c6abc34a53ae47874c31d8a7e7ef58ce434e4
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-21 22:13:25 -0300

    Update exercise-017.py

--- module-001/exercise-017.py
+++ module-001/exercise-017.py
@@ -1,5 +1,5 @@
 '''
-Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
 '''
 
 from math import *
@@ -8,3 +8,5 @@ co = float(input('Digite o valor do cateto oposto: '))
 ca = float(input('Digite o valor do cateto adjacente: '))
 
 h = sqrt(co**2 + ca**2)
+
+print('O valor da hipotenusa é {:.2f}.'.format(h))
