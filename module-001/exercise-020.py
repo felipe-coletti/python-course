5c2a99a28d96a0e0523878f85501b2d2453ec290
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-25 21:02:06 -0300

    Update exercise-020.py

--- module-001/exercise-020.py
+++ module-001/exercise-020.py
@@ -2,3 +2,13 @@
 O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 '''
 
+from random import *
+
+a1 = input('Digite o nome do primeiro aluno: ')
+a2 = input('Digite o nome do segunundo aluno: ')
+a3 = input('Digite o nome do terceiro aluno: ')
+a4 = input('Digite o nome do quarto aluno: ')
+
+l = [a1, a2, a3, a4]
+
+print('O aluno sorteado foi {}.'.format(shuffle(l)))
