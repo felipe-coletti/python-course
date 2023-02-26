fb173689957b34af6b97c971543bc35d5ea23286
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-25 21:30:30 -0300

    Update exercise-018.py

--- module-001/exercise-018.py
+++ module-001/exercise-018.py
@@ -2,7 +2,7 @@
 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 '''
 
-from math import *
+from math import radians, sin, cos, tan
 
 a = float(input('Digite o valor de um ângulo: '))
 
