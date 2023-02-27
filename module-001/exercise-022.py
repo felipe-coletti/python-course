7da0241bc6ea4cac72fe707a344e5b1c9d802c3a
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-02-27 13:39:09 -0300

    Create exercise-022.py

---
+++ module-001/exercise-022.py
@@ -0,0 +1,5 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa e mostre: o nome com todas as letras maiúsculas e minúsculas, quantas letras ao todo (sem considerar espaços) e quantas letras tem o primeiro nome.
+'''
+
+n = input('Digite o seu nome completo: ')
