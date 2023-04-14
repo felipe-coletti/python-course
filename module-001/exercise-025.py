efa5da9549972e3d02b753095b95a87c80ae295b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-14 18:07:55 -0300

    Create exercise-025.py

---
+++ module-001/exercise-025.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+'''
+
+name = input('Digite um nome completo: ')
+
+print('O nome tem Silva? {}'.format('silva' in name.lower()))
