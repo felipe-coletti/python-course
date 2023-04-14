00b29d2a00bfcf92b0acd6dc4f42257a94e9fd64
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-14 18:37:01 -0300

    Update exercise-004.py

--- module-001/exercise-004.py
+++ module-001/exercise-004.py
@@ -2,13 +2,13 @@
 Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 '''
 
-x = input('Digite algo: ')
+something = input('Digite algo: ')
 
-print('O tipo primitivo é: {}'.format(type(x)))
-print('É composto apenas por espaços? {}'.format(x.isspace()))
-print('É composto apenas por números? {}'.format(x.isnumeric()))
-print('É composto apenas por letras? {}'.format(x.isalpha()))
-print('É composto apenas por números e letras? {}'.format(x.isalnum()))
-print('É composto apenas por letras maiúsculas? {}'.format(x.isupper()))
-print('É composto apenas por letras minúsculas? {}'.format(x.islower()))
-print('Está capitalizado? {}'.format(x.istitle()))
+print('O tipo primitivo é: {}'.format(type(something)))
+print('É composto apenas por espaços? {}'.format(something.isspace()))
+print('É composto apenas por números? {}'.format(something.isnumeric()))
+print('É composto apenas por letras? {}'.format(something.isalpha()))
+print('É composto apenas por números e letras? {}'.format(something.isalnum()))
+print('É composto apenas por letras maiúsculas? {}'.format(something.isupper()))
+print('É composto apenas por letras minúsculas? {}'.format(something.islower()))
+print('Está capitalizado? {}'.format(something.istitle()))
