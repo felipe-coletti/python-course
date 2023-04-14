5f8dc4fe5db7f802fb82a60b7827e667ce62d8d3
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-14 15:08:59 -0300

    Update exercise-006.py

--- module-001/exercise-006.py
+++ module-001/exercise-006.py
@@ -2,7 +2,7 @@
 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 '''
 
-n = int(input('Digite um número: '))
+n = float(input('Digite um número: '))
 
 d = n * 2
 t = n * 3
