b7f53de4d2435b2cae5b5e23dde6144fe84a44a4
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-15 10:13:31 -0300

    Update exercise-013.py

--- module-001/exercise-013.py
+++ module-001/exercise-013.py
@@ -6,4 +6,4 @@ s = float(input('Digite o salário de um funcionário: R$ '))
 
 n = s + (15 / 100 * s)
 
-print('O salário do funcionário, com um aumento de 15%, vai de R$ {:.2f} para R$ {:.2f}.'.format(p, n))
+print('O salário do funcionário, com um aumento de 15%, vai de R$ {:.2f} para R$ {:.2f}.'.format(s, n))
