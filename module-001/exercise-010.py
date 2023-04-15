4d42781bd7fd899015e7c9ff2259d4b48a351464
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-15 00:26:43 -0300

    Update exercise-010.py

--- module-001/exercise-010.py
+++ module-001/exercise-010.py
@@ -2,8 +2,8 @@
 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere USD$ 1,00 = R$ 3,27.
 '''
 
-r = float(input('Digite um valor em reais: '))
+real = float(input('Digite um valor em reais: '))
 
-d = r / 3.27
+dollar = real / 3.27
 
-print('R$ {:.2f} equivale a USD$ {:.2f}.'.format(r, d))
+print('R$ {:.2f} equivale a USD$ {:.2f}.'.format(real, dollar))
