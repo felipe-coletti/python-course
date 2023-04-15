11e20f4202b2b98a7f78aed19b873a078dd0f1cb
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-14 23:00:04 -0300

    Update exercise-009.py

--- module-001/exercise-009.py
+++ module-001/exercise-009.py
@@ -2,16 +2,16 @@
 Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 '''
 
-n = int(input('Digite um número inteiro: '))
+number = int(input('Digite um número inteiro: '))
 
-print('\n{} x 0 = {}'.format(n, n * 0))
-print('{} x 1 = {}'.format(n, n * 1))
-print('{} x 2 = {}'.format(n, n * 2))
-print('{} x 3 = {}'.format(n, n * 3))
-print('{} x 4 = {}'.format(n, n * 4))
-print('{} x 5 = {}'.format(n, n * 5))
-print('{} x 6 = {}'.format(n, n * 6))
-print('{} x 7 = {}'.format(n, n * 7))
-print('{} x 8 = {}'.format(n, n * 8))
-print('{} x 9 = {}'.format(n, n * 9))
-print('{} x 10 = {}'.format(n, n * 10))
+print('\n{} x 0 = {}'.format(number, number * 0))
+print('{} x 1 = {}'.format(number, number * 1))
+print('{} x 2 = {}'.format(number, number * 2))
+print('{} x 3 = {}'.format(number, number * 3))
+print('{} x 4 = {}'.format(number, number * 4))
+print('{} x 5 = {}'.format(number, number * 5))
+print('{} x 6 = {}'.format(number, number * 6))
+print('{} x 7 = {}'.format(number, number * 7))
+print('{} x 8 = {}'.format(number, number * 8))
+print('{} x 9 = {}'.format(number, number * 9))
+print('{} x 10 = {}'.format(number, number * 10))
