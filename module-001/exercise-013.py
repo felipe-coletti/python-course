b4aef3a12b03e0a80c17ea935f385e6b29ee67db
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-16 20:59:12 -0300

    Update exercise-013.py

--- module-001/exercise-013.py
+++ module-001/exercise-013.py
@@ -2,8 +2,8 @@
 Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 '''
 
-s = float(input('Digite o salário de um funcionário: R$ '))
+salary = float(input('Digite o salário de um funcionário: R$ '))
 
-n = s + (15 / 100 * s)
+newSalary = salary + (15 / 100 * salary)
 
-print('O salário do funcionário, com um aumento de 15%, vai de R$ {:.2f} para R$ {:.2f}.'.format(s, n))
+print('O salário do funcionário, com um aumento de 15%, vai de R$ {:.2f} para R$ {:.2f}.'.format(salary, newSalary))
