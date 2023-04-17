13404df0fddf595089964a423c07a57b5d941542
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-17 00:00:07 -0300

    Update exercise-024.py

--- module-001/exercise-024.py
+++ module-001/exercise-024.py
@@ -1,5 +1,5 @@
 '''
-Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "Santo".
 '''
 
 name = input('Digite o nome de uma cidade: ')
