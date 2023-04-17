ca63646f2877231a4ec64408c0be82d0ccfa6363
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-17 20:06:02 -0300

    Update exercise-024.py

--- module-001/exercise-024.py
+++ module-001/exercise-024.py
@@ -4,6 +4,8 @@ Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o
 
 name = input('Digite o nome de uma cidade: ')
 
-firstName = name.split()[0]
+names = name.split()
+
+firstName = names[0]
 
 print('O nome da cidade começa com Santo? {}'.format(firstName.lower() == 'santo'))
