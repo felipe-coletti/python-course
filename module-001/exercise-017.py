c9240905ff95a643a6dc57ab2a94c3752b98171d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-04-19 21:55:31 -0300

    Update exercise-017.py

--- module-001/exercise-017.py
+++ module-001/exercise-017.py
@@ -4,9 +4,9 @@ Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente 
 
 from math import sqrt
 
-co = float(input('Digite o valor do cateto oposto: '))
-ca = float(input('Digite o valor do cateto adjacente: '))
+aSide = float(input('Digite o valor do cateto oposto: '))
+bSide = float(input('Digite o valor do cateto adjacente: '))
 
-h = sqrt(co**2 + ca**2)
+hypotenuse = sqrt(aSide**2 + bSide**2)
 
-print('O valor da hipotenusa é {:.2f}.'.format(h))
+print('O valor da hipotenusa é {:.2f}.'.format(hypotenuse))
