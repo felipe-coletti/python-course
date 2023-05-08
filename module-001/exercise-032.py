899e40eac74a09c98bec15554ad486bfc4361dcc
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-08 12:13:15 -0300

    Update exercise-032.py

--- module-001/exercise-032.py
+++ module-001/exercise-032.py
@@ -2,7 +2,7 @@
 Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 '''
 
-year = input('Digite uma ano: ')
+year = input('Digite um ano: ')
 
 if year & 4 = 0 and year & 100 != 0 or year & 400 = 0:
     print('{} é um ano bissexto.'.format(year))
