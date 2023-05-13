ab3b2f2563ec253b747c4d6a2f161a656cdb4c13
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 18:56:51 -0300

    Create exercise-033.py

---
+++ module-001/exercise-033.py
@@ -0,0 +1,23 @@
+'''
+Faça um programa que leia três números e mostre qual é o menor e qual é o maior.
+'''
+
+firstNumber = float(input('Digite o primeiro número: '))
+secondNumber = float(input('Digite o segundo número: '))
+thirdNumber = float(input('Digite o terceiro número: '))
+
+smallerNumber = firstNumber
+
+if secondNumber < smallerNumber:
+    smallerNumber = secondNumber
+if thirdNumber < smallerNumber:
+    smallerNumber = thirdNumber
+
+higherNumber = firstNumber
+
+if secondNumber > higherNumber:
+    higherNumber = secondNumber
+if thirdNumber > higherNumber:
+    higherNumber = thirdNumber
+
+print('O menor número é {} e o maior número é {}.'.format(smallerNumber, higherNumber))
