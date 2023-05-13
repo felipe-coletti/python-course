7a7e132d7a145e7e335c518585c6b31df20941e0
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-13 19:21:59 -0300

    Update exercise-012.py

--- module-001/exercise-012.py
+++ module-001/exercise-012.py
@@ -4,6 +4,6 @@ Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com
 
 price = float(input('Digite o preço do produto: R$ '))
 
-discountedPrice = price - (5 / 100 * price)
+discountedPrice = price - 5 / 100 * price
 
 print('O preço do produto, com um desconto de 5%, vai de R$ {:.2f} para R$ {:.2f}.'.format(price, discountedPrice))
