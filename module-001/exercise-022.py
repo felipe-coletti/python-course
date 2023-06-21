920dd1a57d53bf14e7463a0726da100c5a267f6d
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-21 19:36:59 -0300

    Update exercise-022.py

--- module-001/exercise-022.py
+++ module-001/exercise-022.py
@@ -2,13 +2,13 @@
 Crie um programa que leia o nome completo de uma pessoa e mostre: o nome com todas as letras maiúsculas e minúsculas, quantas letras ao todo (sem considerar espaços) e quantas letras tem o primeiro nome.
 '''
 
-name = input('Digite um nome completo: ')
+fullName = input('Digite um nome completo: ')
 
-names = name.split()
+names = fullName.split()
 
 firstName = names[0]
 
-print('O nome em letras maiúsculas é {}.'.format(name.upper()))
-print('O nome em letras minúsculas é {}.'.format(name.lower()))
-print('O nome tem {} letras.'.format(len(name.replace(' ', ''))))
+print('O nome em letras maiúsculas é {}.'.format(fullName.upper()))
+print('O nome em letras minúsculas é {}.'.format(fullName.lower()))
+print('O nome tem {} letras.'.format(len(fullName.replace(' ', ''))))
 print('O primeiro nome é {} e ele tem {} letras.'.format(firstName, len(firstName)))
