aab0c21996be9285ee427285d8c1a4e42000a49f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-21 19:33:17 -0300

    Update exercise-025.py

--- module-001/exercise-025.py
+++ module-001/exercise-025.py
@@ -2,6 +2,6 @@
 Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
 '''
 
-name = input('Digite um nome completo: ')
+fullName = input('Digite um nome completo: ')
 
-print('O nome tem Silva? {}'.format('silva' in name.lower()))
+print('O nome tem Silva? {}'.format('silva' in fullName.lower()))
