1f37b1211444381573fc9a21832389a0346ff8d4
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-24 13:15:36 -0300

    Update exercise-004.py

--- module-002/exercise-004.py
+++ module-002/exercise-004.py
@@ -2,9 +2,13 @@
 Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 '''
 
+from datetime import date
+
 birthYear = int(input('Digite o ano de nascimento: '))
 
-yearsOld = 2023 - birthYear
+currentYear = date.today().year
+
+yearsOld = currentYear - birthYear
     
 if yearsOld > 18:
     lagTime = yearsOld - 18
@@ -14,13 +18,13 @@ if yearsOld > 18:
     else:
         print('O alistamento foi a {} anos.'.format(lagTime))
 elif yearsOld == 18:
-    print('O alistamento é nesse ano.')
+    print('O alistamento é esse ano.')
 elif yearsOld < 18 and yearsOld >= 0:
     timeLeft = 18 - yearsOld
     
     if timeLeft == 1:
-        print('Falta {} ano até o alistamento.'.format(timeLeft))
+        print('Falta {} ano para o alistamento.'.format(timeLeft))
     else:
-        print('Faltam {} anos até o alistamento.'.format(timeLeft))
+        print('Faltam {} anos para o alistamento.'.format(timeLeft))
 else:
     print('Ano de nascimento inválido.')
