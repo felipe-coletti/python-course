256f24581b34f085f3a3012b16d64b18357a6059
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-24 14:24:48 -0300

    Create exercise-007.py

---
+++ module-002/exercise-007.py
@@ -0,0 +1,19 @@
+'''
+Refaça o desafio 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: equilátero (quando todos os lados são iguais), isósceles (quando só dois lados são iguais) ou escaleno (quando todos os lados são diferentes).
+'''
+
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valor de c: '))
+
+if a < b + c and b < a + c and c < a + b:
+    print('Os segmentos de reta a, b e c podem formar um triângulo.')
+    
+    if a == b == c:
+        print('O triângulo formado seria um triângulo equilátero.')
+    elif a != b != c:
+        print('O triângulo formado seria um triângulo escaleno.')
+    else:
+        print('O triângulo formado seria um triângulo isósceles.')
+else:
+    print('Os segmentos de reta a, b e c não podem formar um triângulo.')
