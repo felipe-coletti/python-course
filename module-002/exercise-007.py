e1d2dd1c37fb7f27b571954f777840b9c9acaafd
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-06-24 14:32:18 -0300

    Update exercise-007.py

--- module-002/exercise-007.py
+++ module-002/exercise-007.py
@@ -9,7 +9,7 @@ c = float(input('Digite o valor de c: '))
 if a < b + c and b < a + c and c < a + b:
     if a == b == c:
         print('Os segmentos de reta a, b e c podem formar um triângulo equilátero.')
-    elif a != b != c:
+    elif a != b != c != a:
         print('Os segmentos de reta a, b e c podem formar um triângulo escaleno.')
     else:
         print('Os segmentos de reta a, b e c podem formar um triângulo isósceles.')
