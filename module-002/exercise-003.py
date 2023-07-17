f49319d1105c9ffd980eca03c2f1d90f074bdb7e
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-07-17 17:43:57 -0300

    Update exercise-003.py

--- module-002/exercise-003.py
+++ module-002/exercise-003.py
@@ -1,8 +1,8 @@
 '''
 Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
-- O primeiro valor é maior
-- O segundo valor é maior
-- Não existe valor maior, os dois são iguais
+- O primeiro valor é maior;
+- O segundo valor é maior;
+- Não existe valor maior, os dois são iguais.
 '''
 
 firstNumber = int(input('Digite o primeiro número: '))
