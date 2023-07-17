d724795a562db75b4c6b77fcf72188e444672264
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-07-17 17:54:15 -0300

    Update exercise-007.py

--- module-002/exercise-007.py
+++ module-002/exercise-007.py
@@ -1,5 +1,8 @@
 '''
-Refaça o desafio 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: equilátero (quando todos os lados são iguais), isósceles (quando só dois lados são iguais) ou escaleno (quando todos os lados são diferentes).
+Refaça o desafio 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+- Todos os lados iguais: equilátero;
+- Dois lados iguais e um diferente: isósceles;
+- Todos os lados diferentes: escaleno.
 '''
 
 a = float(input('Digite o valor de a: '))
