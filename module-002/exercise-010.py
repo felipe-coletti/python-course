ee80f58d80f6f6e1f731cb5aaf61a6ccc3497dd3
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-08-19 13:58:50 -0300

    Create exercise-010.py

---
+++ module-002/exercise-010.py
@@ -0,0 +1,19 @@
+from random import randint
+
+options = ['Pedra', 'Papel', 'Tesoura']
+
+firstOption = int(input('Escolha uma opção:\n[1] Pedra.\n[2] Papel.\n[3] Tesoura.'))
+
+secondOption = randint(1, 3)
+
+if 0 < firstOption < 4:
+    print('Jogador 1 escolheu {}.\nJogador 2 escolheu {}.'.format(options[firstOption - 1], options[secondOption - 1]))
+    
+    if firstOption - 1 == secondOption or firstOption == 1 and secondOption == 3:
+        print('Você ganhou.')
+    elif secondOption - 1 == firstOption or secondOption == 1 and firstOption == 3:
+        print('Você perdeu.')
+    else:
+        print('Empate.')
+else:
+    print('Opção inválida.')
