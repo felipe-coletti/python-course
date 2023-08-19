996c9859bb0a9d9edb38bdc2f1db4bf1269a1a67
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-08-19 14:22:02 -0300

    Update exercise-009.py

--- module-002/exercise-009.py
+++ module-002/exercise-009.py
@@ -1,3 +1,11 @@
+'''
+Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+- à vista dinheiro/cheque: 10% de desconto
+- à vista no cartão: 5% de desconto
+- em até 2x no cartão: preço formal
+- 3x ou mais no cartão: 20% de juros
+'''
+
 price = float(input('Digite o preço da compra: R$ '))
 
 paymentMethod = int(input('Formas de pagamento:\n[1] à vista no dinheiro/cheque.\n[2] à vista no cartão.\n[3] 2x no cartão.\n[4] 3x ou mais no cartão.\nSelecione uma opção.'))
