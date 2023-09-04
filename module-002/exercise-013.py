84b8e00944993a5585e1a49937bfd48cd41dde7a
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-09-04 18:00:50 -0300

    Create exercise-013.py

---
+++ module-002/exercise-013.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+'''
+
+sum = 0
+
+for i in range(1, 501):
+    if i % 2 != 0 and i % 3 == 0:
+        sum += i
+        
+print('A soma dos números impares múltiplos de 3 entre 1 e 500 é: {}.'.format(sum))
