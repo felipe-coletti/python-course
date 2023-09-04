397b8068f8d42b7a8d046443afd8dda9f42cb326
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-09-04 18:57:53 -0300

    Create exercise-016.py

---
+++ module-002/exercise-016.py
@@ -0,0 +1,15 @@
+'''
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+'''
+
+a1 = float(input('Digite o primeiro termo da progressão aritmética: '))
+r = float(input('Digite a razão da progressão aritmética: '))
+
+a = a1
+
+print (a)
+
+while a < a1 + r * 9:
+    a += r
+    print(a)
+    
